87e3b7aa0c907a9776978e94ce9024f04696f40f
Jiravat Boonkumnerd <jo06942@gmail.com>
2017-02-22 04:36:01 +0700

    add cross origin *

--- user-service/src/main/java/demo/UserController.java
+++ user-service/src/main/java/demo/UserController.java
@@ -1,6 +1,7 @@
 package demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
