8c00689169a12b3e52921ee849e792481726b653
Pichai Sivawat <tingtong003tomy@gmail.com>
2017-03-05 13:56:15 +0700

    remove unnecessary method

--- post-service/src/main/java/com/shenzhentagram/posts/PostRepository.java
+++ post-service/src/main/java/com/shenzhentagram/posts/PostRepository.java
@@ -1,15 +1,12 @@
 package com.shenzhentagram.posts;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import java.util.List;
 
 /**
  * Created by phompang on 3/5/2017 AD.
  */
 @RepositoryRestResource(collectionResourceRel = "post", path = "posts")
 public interface PostRepository extends PagingAndSortingRepository<Post, Long> {
-    List<Post> findById(@Param("id") long id);
+
 }
