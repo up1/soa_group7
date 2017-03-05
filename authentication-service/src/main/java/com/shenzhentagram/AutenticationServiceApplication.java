7ca2949f7cc78f62ed9abbf5b06dc1a21e6e8614
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2017-03-05 14:42:01 +0700

    Initial Auth service project

---
+++ authentication-service/src/main/java/com/shenzhentagram/AutenticationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.shenzhentagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticationServiceApplication.class, args);
+	}
+}
