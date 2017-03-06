f8d45d0ab7a1ec795e7ecc601f0b9e1e8b5c572b
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2017-03-06 22:27:25 +0700

    Update controller (resources)

--- user-service/src/main/java/com/shenzhentagram/UserController.java
+++ user-service/src/main/java/com/shenzhentagram/UserController.java
@@ -1,13 +1,16 @@
 package com.shenzhentagram;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shenzhentagram.authentication.AuthenticatedUser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -16,25 +19,48 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/users/{user_id}")
-    public User getUser(@PathVariable("user_id") int id) {
-        return this.userRepository.findById((long) id);
+    @RequestMapping(method = RequestMethod.GET , path = "/users/{user_id}", produces = { MediaType.APPLICATION_JSON_VALUE })
+    public User getUser(
+            @PathVariable("user_id") long id
+    ) {
+        return this.userRepository.findById(id);
     }
 
-    @RequestMapping("/users")
-    public List<User> getUsers(
-            @RequestParam(value="page", defaultValue="1") int page,
-            @RequestParam(value="item_per_page", defaultValue="10") int itemPerPage) {
-        return this.userRepository.findAll(page, itemPerPage);
+    @RequestMapping(method = RequestMethod.GET , path = "/users/search", produces = { MediaType.APPLICATION_JSON_VALUE })
+    public List<User> searchUser(
+            @RequestParam("name") String name
+    ) {
+        // TODO Implement search user
+        throw new NotImplementedException();
     }
 
-    @RequestMapping("/users/create")
-    public void saveUser(@RequestParam(value="id") int id,
-                         @RequestParam(value="firstname") String firstname,
-                         @RequestParam(value="lastname") String lastname){
-        User user = new User(id);
-        user.setFirstname(firstname);
-        user.setLastname(lastname);
-        this.userRepository.save(user);
+    @RequestMapping(method = RequestMethod.POST, path = "/users", consumes = { MediaType.APPLICATION_JSON_VALUE })
+    public void createUser(
+            HttpServletResponse response,
+            @RequestBody Map<String, Object> payload
+    ){
+        // Extract the password
+        String password = (String) payload.remove("password");
+
+        // Save new user
+        ObjectMapper mapper = new ObjectMapper();
+        User user = mapper.convertValue(payload, User.class);
+        this.userRepository.save(user, password);
+
+        // Response 201
+        response.setStatus(HttpServletResponse.SC_CREATED);
     }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/users/self", produces = { MediaType.APPLICATION_JSON_VALUE })
+    public User getSelf() {
+        AuthenticatedUser auth = (AuthenticatedUser) SecurityContextHolder.getContext().getAuthentication();
+        return this.userRepository.findById(auth.getId());
+    }
+
+    @RequestMapping(method = { RequestMethod.PUT, RequestMethod.PATCH }, path = "/users/self", produces = { MediaType.APPLICATION_JSON_VALUE })
+    public void updateSelf() {
+        // TODO Implement update self profile
+        throw new NotImplementedException();
+    }
+
 }
