806fe9b64dc4ac0109c9a9fecac861fb5ab4d9b4
Pichai Sivawat <tingtong003tomy@gmail.com>
2017-03-10 00:40:51 +0700

    vue-auth

---
+++ shenzhentagram/src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Hellow</h1>
+</template>
+
+<script type="application/ecmascript">
+  export default {
+      data () {
+          return {}
+      }
+  }
+</script>
--- shenzhentagram/src/router/index.js
+++ shenzhentagram/src/router/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Home from '@/components/Home'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueAuth from '@websanova/vue-auth'
 
-Vue.use(Router)
-
-export default new Router({
+const router = new Router({
+  hashbang: false,
+  linkActiveClass: 'active',
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Hello',
-      component: Hello
+      component: Hello,
+      meta: {auth: false}
+    }, {
+      path: '/home',
+      name: 'Home',
+      component: Home
     }
   ]
 })
+
+Vue.router = router
+Vue.use(Router)
+Vue.use(VueAxios, axios)
+Vue.use(VueAuth, {
+  auth: require('@websanova/vue-auth/drivers/auth/bearer.js'),
+  http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
+  router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js')
+})
+
+export default router
