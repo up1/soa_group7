1b38580c5761c768c4007a3a01248602a4b602b9
Pichai Sivawat <tingtong003tomy@gmail.com>
2017-03-18 16:49:47 +0700

    Post card

---
+++ shenzhentagram/src/components/Card.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image is-1by1">
+        <img src="http://placehold.it/960x960.png" alt="Image">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left">
+          <figure class="image is-48x48">
+            <img class="profile-img" src="http://bulma.io/images/placeholders/96x96.png" alt="Image">
+          </figure>
+        </div>
+        <div class="media-content">
+          <p class="title is-4">John Smith</p>
+          <p class="subtitle is-6">@johnsmith</p>
+        </div>
+      </div>
+
+      <div class="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+        <a>#css</a> <a>#responsive</a>
+        <br>
+        <small>11:09 PM - 1 Jan 2016</small>
+      </div>
+
+      <footer class="card-footer">
+        <a href="" class="footer-item"><i class="fa fa-gratipay fa-2x" aria-hidden="true"></i></a>
+        <span class="footer-item">
+          <div class="field">
+            <p class="control">
+              <input class="input is-medium" type="text" placeholder="Add a comment...">
+            </p>
+          </div>
+        </span>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript">
+  export default {
+      data() {
+          return {}
+      }
+  }
+</script>
+
+<style scroped>
+  .profile-img {
+    border-radius: 50%;
+  }
+  .footer-item {
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    padding: 0.75rem 0.75rem 0 0.75rem;
+  }
+
+  .input {
+    border: none;
+    box-shadow: none;
+  }
+</style>
--- shenzhentagram/src/components/Home.vue
+++ shenzhentagram/src/components/Home.vue
@@ -1,12 +1,28 @@
 <template>
-  <h1>Hellow</h1>
+  <div class="container home">
+    <div class="columns">
+      <div class="column is-half is-offset-one-quarter">
+        <card></card>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script type="application/ecmascript">
+  import Card from './Card'
   export default {
       name: 'home',
+      components: {
+          Card
+      },
       data () {
           return {}
       }
   }
 </script>
+
+<style scoped>
+  .home {
+    margin-top: 50px;
+  }
+</style>
