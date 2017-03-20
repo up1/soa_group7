f04d0cc458bf21d4e5a6de20933aace4e66fea77
Pichai Sivawat <tingtong003tomy@gmail.com>
2017-03-20 19:04:33 +0700

    Docker

--- post-service/src/test/java/com/shenzhentagram/PostServiceApplicationTests.java
+++ post-service/src/test/java/com/shenzhentagram/PostServiceApplicationTests.java
@@ -1,26 +1,5 @@
 package com.shenzhentagram;
 
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = PostServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestPropertySource(properties = {"management.port=9012"})
 public class PostServiceApplicationTests {
-
-	@LocalServerPort
-	private int port;
-
-	@Value("${local.management.port}")
-	private int mgt;
-
-	@Autowired
-	private TestRestTemplate testRestTemplate;
 
 }
