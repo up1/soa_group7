8f158e91bf521cf3828e46b367df734ef07bb471
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2017-03-20 19:38:01 +0700

    Add post service route
    NOTE: post service not finish yet (no auth token send with request when calling)

--- api-gateway/src/main/java/com/shenzhentagram/controller/AuthenticationController.java
+++ api-gateway/src/main/java/com/shenzhentagram/controller/AuthenticationController.java
@@ -11,13 +11,14 @@ import org.springframework.web.bind.annotation.*;
  * Created by Meranote on 3/20/2017.
  */
 @RestController
+@RequestMapping(path = "/auth")
 public class AuthenticationController extends TemplateRestController {
 
     public AuthenticationController(Environment environment, RestTemplateBuilder restTemplateBuilder) {
         super(environment, restTemplateBuilder, "authentication");
     }
 
-    @PostMapping(path = "/auth")
+    @PostMapping()
     public AuthenticateDetail auth(
             @RequestParam("username") String username,
             @RequestParam("password") String password
---
+++ api-gateway/src/main/java/com/shenzhentagram/controller/PostController.java
@@ -0,0 +1,66 @@
+package com.shenzhentagram.controller;
+
+import com.shenzhentagram.model.Post;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.env.Environment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Created by Meranote on 3/20/2017.
+ */
+@CrossOrigin
+@RestController
+@RequestMapping(path = "/posts")
+public class PostController extends TemplateRestController {
+
+    // FIXME Extract authorization token
+
+    public PostController(Environment environment, RestTemplateBuilder restTemplateBuilder) {
+        super(environment, restTemplateBuilder, "post");
+    }
+
+    @GetMapping()
+    public Page<Post> getPosts(Pageable pageable) {
+        return restTemplate.getForObject("/posts", Page.class);
+    }
+
+    @GetMapping(value = "/{id}")
+    public Post getPosts(
+            @PathVariable("id") long id
+    ) {
+        return restTemplate.getForObject("/posts/{id}", Post.class, id);
+    }
+
+    @PostMapping()
+    public Post postPost(
+            @RequestParam(value = "caption") String caption,
+            @RequestParam(value = "type") String type,
+            @RequestParam(value = "file") MultipartFile file
+    ) {
+        // TODO Post (upload file and sent to service)
+        throw new NotImplementedException();
+    }
+
+    @PatchMapping(value = "/{id}")
+    public Post patchPost(
+            @PathVariable("id") long id,
+            @RequestParam(value = "caption") String caption
+    ) {
+        // FIXME Send JSON body (post service is accepting query params by now)
+//        return restTemplate.patchForObject("/posts/{id}", new PostUpdateDetail(id, caption), Post.class, id);
+        return restTemplate.patchForObject(String.format("/posts/{id}?id=%d&caption=%s", id, caption), null, Post.class, id);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deletePost(
+            @PathVariable("id") long id
+    ) {
+        // QUESTION Did this need to return the deleted post?
+        restTemplate.delete("/posts/{id}", id);
+    }
+
+}
