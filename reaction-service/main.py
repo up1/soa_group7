5eec11c756ab0ea2d8f712489906cb46020ec089
Pichai Sivawat <tingtong003tomy@gmail.com>
2017-03-27 17:50:54 +0700

    get reactions

--- reaction-service/main.py
+++ reaction-service/main.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, json
+import pymysql
+from flask import Flask, request, json, jsonify
 from flaskext.mysql import MySQL
+
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -12,10 +14,17 @@ mysql.init_app(app)
 
 
 def get_reacts(post_id):
-    raise NotImplemented()
+    try:
+        cursor = mysql.get_db().cursor(pymysql.cursors.DictCursor)
+        sql = "SELECT id, reaction, user_id from reactions where post_id = %s"
+        cursor.execute(sql, post_id)
+        result = cursor.fetchall()
+        return jsonify(result)
+    except Exception as e:
+        return json.dumps({'error': str(e)})
 
 
-def create_react(post_id, user_id):
+def create_react(post_id, user_id, reaction):
     raise NotImplemented()
 
 
@@ -29,22 +38,17 @@ def delete_react(post_id, user_id):
 
 @app.route('/posts/<int:post_id>/reacts', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def reacts(post_id):
-    try:
-        cursor = mysql.get_db().cursor()
-
+    if request.method == 'GET':
+        return get_reacts(post_id)
+    elif request.method == 'POST':
         user_id = request.form['user_id']
-
-        if request.method == 'GET':
-            return get_reacts(post_id)
-        elif request.method == 'POST':
-            return create_react(post_id, user_id)
-        elif request.method == 'PUT':
-            return update_react(post_id, user_id)
-        elif request.method == 'DELETE':
-            return delete_react(post_id, user_id)
-    except Exception as e:
-        return json.dumps({'error': str(e)})
-
+        return create_react(post_id, user_id)
+    elif request.method == 'PUT':
+        user_id = request.form['user_id']
+        return update_react(post_id, user_id)
+    elif request.method == 'DELETE':
+        user_id = request.form['user_id']
+        return delete_react(post_id, user_id)
 
 if __name__ == "__main__":
     app.run(port=9007, debug=True)
