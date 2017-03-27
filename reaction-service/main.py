052c5ef5cca3a39b33fcff6c739e8ef826064866
Pichai Sivawat <tingtong003tomy@gmail.com>
2017-03-27 18:07:01 +0700

    create reaction

--- reaction-service/main.py
+++ reaction-service/main.py
@@ -13,6 +13,13 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 
+def get_react(post_id, user_id):
+    cursor = mysql.get_db().cursor(pymysql.cursors.DictCursor)
+    sql = "SELECT id, reaction, user_id from reactions where post_id = %s AND user_id = %s"
+    cursor.execute(sql, (post_id, user_id))
+    return jsonify(cursor.fetchone())
+
+
 def get_reacts(post_id):
     try:
         cursor = mysql.get_db().cursor(pymysql.cursors.DictCursor)
@@ -25,10 +32,18 @@ def get_reacts(post_id):
 
 
 def create_react(post_id, user_id, reaction):
-    raise NotImplemented()
+    try:
+        cursor = mysql.get_db().cursor()
+        sql = "INSERT INTO reactions(user_id, post_id, reaction) VALUES (%s, %s, %s)"
+        cursor.execute(sql, (user_id, post_id, reaction))
+        mysql.get_db().commit()
+
+        return get_react(post_id, user_id)
+    except Exception as e:
+        return json.dumps({'error': str(e)})
 
 
-def update_react(post_id, user_id):
+def update_react(post_id, user_id, reaction):
     raise NotImplemented()
 
 
@@ -42,10 +57,12 @@ def reacts(post_id):
         return get_reacts(post_id)
     elif request.method == 'POST':
         user_id = request.form['user_id']
-        return create_react(post_id, user_id)
+        reaction = request.form['reaction']
+        return create_react(post_id, user_id, reaction)
     elif request.method == 'PUT':
         user_id = request.form['user_id']
-        return update_react(post_id, user_id)
+        reaction = request.form['reaction']
+        return update_react(post_id, user_id, reaction)
     elif request.method == 'DELETE':
         user_id = request.form['user_id']
         return delete_react(post_id, user_id)
