7bdf2ab89508504150248e110e2ff66680c0fb3b
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2017-04-03 05:06:54 +0700

    Dockerfile and plugins for making an image

--- api-gateway/src/main/java/com/shenzhentagram/model/UserRegisterDetail.java
+++ api-gateway/src/main/java/com/shenzhentagram/model/UserRegisterDetail.java
@@ -8,15 +8,17 @@ public class UserRegisterDetail {
     private String username;
     private String password;
     private String full_name;
+    private String bio;
     private String display_name;
     private String profile_picture;
 
     public UserRegisterDetail() { }
 
-    public UserRegisterDetail(String username, String password, String full_name, String display_name, String profile_picture) {
+    public UserRegisterDetail(String username, String password, String full_name, String bio, String display_name, String profile_picture) {
         this.username = username;
         this.password = password;
         this.full_name = full_name;
+        this.bio = bio;
         this.display_name = display_name;
         this.profile_picture = profile_picture;
     }
@@ -43,6 +45,14 @@ public class UserRegisterDetail {
 
     public void setFull_name(String full_name) {
         this.full_name = full_name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
     }
 
     public String getDisplay_name() {
