210ff5bffe1fbc9e579d4c921f6223387ab2cd34
PhompAng <tingtong003tomy@gmail.com>
2017-04-14 18:20:33 +0700

    fix typo

--- update_deploy.sh
+++ update_deploy.sh
@@ -2,7 +2,7 @@
 
 docker push meranote/api-gateway
 docker push meranote/authentication-service
-docker push meranote/user-servics
+docker push meranote/user-service
 docker push meranote/post-service
 
 gcloud container clusters get-credentials shenzhentagram-group-asia --zone asia-east1-b --project shenzhentagram
