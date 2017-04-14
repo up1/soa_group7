64162c2104226b36b486f895600159fccffe7753
PhompAng <tingtong003tomy@gmail.com>
2017-04-14 12:52:26 +0700

    build script

--- authentication-service/src/main/java/com/shenzhentagram/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/shenzhentagram/AuthenticationServiceApplication.java
@@ -3,19 +3,12 @@ package com.shenzhentagram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @CrossOrigin(origins = "*")
 public class AuthenticationServiceApplication {
-
-	@RequestMapping(method = RequestMethod.GET, path = "/")
-	public String showHello() {
-		return "Hello!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationServiceApplication.class, args);
