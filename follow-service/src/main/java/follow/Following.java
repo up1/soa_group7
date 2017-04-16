44b074bc6cc9591842863a4eac20f0be5173d953
EndIess <57070115@kmitl.ac.th>
2017-04-16 14:16:42 +0700

    set following

--- follow-service/src/main/java/follow/Following.java
+++ follow-service/src/main/java/follow/Following.java
@@ -11,6 +11,35 @@ import java.util.List;
 
 @Document(collection = "following")
 public class Following {
-    
+    @Id
+    String id;
+
+    long userId;
+    List<User> users;
+
+    public Following() {
+    }
+
+    public Following(long userId, List<User> users) {
+        this.userId = userId;
+        this.users = users;
+
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
 }
 
