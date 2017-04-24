da0e2ea81643923730600df0cdd9c5ac3aa6842c
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2017-04-25 03:35:29 +0700

    Fetch comment, make comment

---
+++ shenzhentagram/src/components/CardComment.vue
@@ -0,0 +1,32 @@
+<!-- TODO implement https://github.com/EvanHahn/HumanizeDuration.js for duration -->
+
+<template>
+  <article class="media comment">
+    <figure class="media-left">
+      <figure class="image is-48x48">
+        <img class="profile-img" src="http://bulma.io/images/placeholders/96x96.png" alt="Image">
+      </figure>
+    </figure>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong>{{this.comment.user.display_name}}</strong> <small>@{{this.comment.user.username}}</small> <small>{{this.comment.createdAt | moment("MMM D, YYYY, h:mm A")}}</small>
+          <br>
+          {{this.comment.text}}
+        </p>
+      </div>
+    </div>
+    <!-- <div class="media-right">
+      <button class="delete"></button>
+    </div> -->
+  </article>
+</template>
+
+<script type="text/babel">
+  export default {
+    props: ['comment'],
+    data () {
+      return {}
+    }
+  }
+</script>
