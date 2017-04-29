0e39215358b9df9e9042bbf9bb3f6bb7b0d4c476
PhompAng <tingtong003tomy@gmail.com>
2017-04-29 22:28:03 +0700

    fix typo

--- api-gateway/src/main/java/com/shenzhentagram/controller/PostController.java
+++ api-gateway/src/main/java/com/shenzhentagram/controller/PostController.java
@@ -13,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @CrossOrigin
@@ -135,7 +134,7 @@ public class PostController extends TemplateRestController {
         // Remove all post's comments
         commentController.deleteCommentsOfPostId(Math.toIntExact(id));
 
-        return responseEntity;
+        return response;
     }
 
     /**
