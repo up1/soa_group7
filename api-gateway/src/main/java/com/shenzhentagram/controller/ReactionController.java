7d9bd597b643058057df06b306a9bf5719046071
EndIess <57070115@kmitl.ac.th>
2017-05-01 17:54:18 +0700

    edit ReactionController

--- api-gateway/src/main/java/com/shenzhentagram/controller/ReactionController.java
+++ api-gateway/src/main/java/com/shenzhentagram/controller/ReactionController.java
@@ -3,6 +3,7 @@ package com.shenzhentagram.controller;
 import com.shenzhentagram.model.Reaction;
 import com.shenzhentagram.model.ReactionBase;
 import com.shenzhentagram.model.ReactionList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
@@ -18,7 +19,13 @@ import org.springframework.web.bind.annotation.*;
 public class ReactionController extends TemplateRestController {
 
     private static final String BASE_URL = "/posts/{post_id}/reactions";
-    
+
+    @Autowired
+    private NotificationController notificationController;
+
+    @Autowired
+    private PostController postController;
+
     /**
      * Setup the template for REST request
      *
@@ -42,7 +49,16 @@ public class ReactionController extends TemplateRestController {
             @RequestBody ReactionBase reaction
     ) {
         reaction.setUserId(getAuthenticatedUser().getId());
-        return request(HttpMethod.POST, BASE_URL, reaction, Reaction.class, postId);
+
+        ResponseEntity<Reaction> responseEntity = request(HttpMethod.POST, BASE_URL, reaction, Reaction.class, postId);
+
+        // Increase post comment count
+        postController.increaseReactions(postId);
+
+        // Create comment notification
+        notificationController.createReactionNotification(Math.toIntExact(getAuthenticatedUser().getId()), postId, responseEntity.getBody().getId());
+
+        return responseEntity;
     }
 
     @PutMapping("/{post_id}/reactions")
@@ -55,13 +71,17 @@ public class ReactionController extends TemplateRestController {
     }
 
     @DeleteMapping("/{post_id}/reactions")
-    public ResponseEntity<Reaction> deleteReaction(
+    public ResponseEntity<Void> deleteReaction(
             @PathVariable("post_id") int postId
     ) {
         ReactionBase reaction = new ReactionBase();
         reaction.setUserId(getAuthenticatedUser().getId());
 
-        return request(HttpMethod.DELETE, BASE_URL, reaction, Reaction.class, postId);
+        ResponseEntity<Reaction> responseEntity = request(HttpMethod.DELETE, BASE_URL, reaction, Reaction.class, postId);
+
+        postController.decreaseReactions(postId);
+
+        return new ResponseEntity<>(responseEntity.getStatusCode());
     }
 
 }
