9bf04fa331b66237c0c33b4bdf6665ae3968fd1e
EndIess <57070115@kmitl.ac.th>
2017-05-01 22:37:42 +0700

    Revert "Return empty array if user not exists in mongodb (instead of nothing)"
    This reverts commit 313be7c52351451516020df478185979c25049fd.

--- api-gateway/src/main/java/com/shenzhentagram/controller/FollowController.java
+++ api-gateway/src/main/java/com/shenzhentagram/controller/FollowController.java
@@ -1,128 +1,94 @@
-package follow;
+package com.shenzhentagram.controller;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import com.shenzhentagram.model.FollowIds;
+import com.shenzhentagram.model.Follower;
+import com.shenzhentagram.model.Following;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.HashMap;
 
-@CrossOrigin
+/**
+ * Created by Meranote on 4/27/2017.
+ */
 @RestController
+@CrossOrigin
 @RequestMapping("/users")
-public class FollowController {
-
-    /**
-     * Logging
-     */
-    protected Log logger = LogFactory.getLog(FollowController.class);
+public class FollowController extends TemplateRestController {
 
     @Autowired
-    private FollowsRepository followsRepository;
+    private UserController userController;
 
-    private static final String USERID = "userId";
-
-    @GetMapping("/{id}/follows")
-    public Follows getFollows(@PathVariable("id") String id) {
-        Follows follows = followsRepository.findById(id);
-
-        if(follows == null) {
-            follows = new Follows();
-            follows.setFollower(new ArrayList<>());
-            follows.setFollowing(new ArrayList<>());
-        }
-
-        return follows;
+    /**
+     * Setup the template for REST request
+     *
+     * @param environment
+     * @param restTemplateBuilder
+     */
+    public FollowController(Environment environment, RestTemplateBuilder restTemplateBuilder) {
+        super(environment, restTemplateBuilder, "follow");
     }
 
+    @GetMapping("/{id}/follower")
+    public ResponseEntity<Follower> getFollower(
+            @PathVariable("id") int userId
+    ) {
+        ResponseEntity<FollowIds> responseEntity = request(HttpMethod.GET, "/users/{id}/follows", FollowIds.class, userId);
+
+        // Convert ids to users
+        Follower follower = userController.convertFollowerIds(responseEntity.getBody().getFollower());
+
+        return new ResponseEntity<>(follower, responseEntity.getStatusCode());
+    }
+
+    @GetMapping("/{id}/following")
+    public ResponseEntity<Following> getFollowing(
+            @PathVariable("id") int userId
+    ) {
+        ResponseEntity<FollowIds> responseEntity = request(HttpMethod.GET, "/users/{id}/follows", FollowIds.class, userId);
+
+        // Convert ids to users
+        Following following = userController.convertFollowingIds(responseEntity.getBody().getFollowing());
+
+        return new ResponseEntity<>(following, responseEntity.getStatusCode());
+    }
 
     @PostMapping("/{id}/follows")
-    public Follows createFollowing   (@PathVariable("id") String id, @RequestBody Map<String, Object> payload) {
-        List<Integer> follower = new ArrayList<>();
-        List<Integer> following = new ArrayList<>();
+    public ResponseEntity<Void> followTo(
+            @PathVariable("id") int userId
+    ) {
+        HashMap<String, Object> sendPayload = new HashMap<String, Object>() {{
+            put("userId", getAuthenticatedUser().getId());
+        }};
 
-        ArrayList<Integer> useradd = new ArrayList<>();
+        // Increase follower to target {user_id}
+        userController.increaseFollower(userId);
 
-        Follows follows;
-        follows = followsRepository.findById(Integer.toString((Integer)payload.get(USERID)));
-        try {
-            following = follows.getFollowing();
-            Set<Integer> mySet = new HashSet<>(following);
-            mySet.add(Integer.parseInt(id));
-            useradd.addAll(mySet);
-            follows.setFollowing(useradd);
-            followsRepository.save(follows);
-        }catch (Exception e){
-            logger.debug(e);
-            following.add(Integer.parseInt(id));
-            follows = new Follows(Integer.toString((Integer) payload.get(USERID)), follower ,following);
-            followsRepository.save(follows);
+        // Increase following to current authenticated user
+        userController.increaseFollowing(getAuthenticatedUser().getId());
 
-        }
-
-        Follows follows2;
-        follows2 = followsRepository.findById(id);
-        follower = new ArrayList<>();
-        following = new ArrayList<>();
-        useradd = new ArrayList<>();
-        try {
-            follower = follows2.getFollower();
-            Set<Integer> mySet = new HashSet<>(follower);
-            mySet.add((int)payload.get(USERID));
-            useradd.addAll(mySet);
-            follows2.setFollower(useradd);
-            followsRepository.save(follows2);
-        }catch (Exception e){
-            logger.debug(e);
-            follower.add((int)payload.get(USERID));
-            follows2 = new Follows(id, follower ,following);
-            followsRepository.save(follows2);
-
-        }
-
-        return follows;
-
+        return request(HttpMethod.POST, "/users/{id}/follows", sendPayload, Void.class, userId);
     }
 
-
-
     @DeleteMapping("/{id}/follows")
-    public Follows deleteFollows   (@PathVariable("id") String id, @RequestBody Map<String, Object> payload) {
-        List<Integer> follower;
-        List<Integer> following;
+    public ResponseEntity<Void> unfollowTo(
+            @PathVariable("id") int userId
+    ) {
+        HashMap<String, Object> sendPayload = new HashMap<String, Object>() {{
+            put("userId", getAuthenticatedUser().getId());
+        }};
 
-        ArrayList<Integer> useradd = new ArrayList<>();
+        // Increase follower to target {user_id}
+        userController.decreaseFollower(userId);
 
-        Follows follows;
-        follows = followsRepository.findById(Integer.toString((Integer)payload.get(USERID)));
-        try {
-            following = follows.getFollowing();
-            Set<Integer> mySet = new HashSet<>(following);
-            mySet.remove(Integer.parseInt(id));
-            useradd.addAll(mySet);
-            follows.setFollowing(useradd);
-            followsRepository.save(follows);
-        }catch (Exception e){
-            logger.debug(e);
-        }
+        // Increase following to current authenticated user
+        userController.decreaseFollowing(getAuthenticatedUser().getId());
 
-        useradd = new ArrayList<>();
-        Follows follows2;
-        follows2 = followsRepository.findById(id);
-        try {
-            follower = follows2.getFollower();
-            Set<Integer> mySet = new HashSet<>(follower);
-            mySet.remove(payload.get(USERID));
-            useradd.addAll(mySet);
-            follows2.setFollower(useradd);
-            followsRepository.save(follows2);
-
-        }catch (Exception e){
-            logger.debug(e);
-        }
-
-        return follows;
-
+        return request(HttpMethod.DELETE, "/users/{id}/follows", sendPayload, Void.class, userId);
     }
 
 }
